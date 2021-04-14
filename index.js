05cb1df916339b3a1677d1e5de5dbac90247bf53
Luis Correa <luiscorreacespedes@gmail.com>
2021-04-13 22:33:55 -0400

    Added functions for Owlsay

---
+++ index.js
@@ -0,0 +1,71 @@
+// Principal function
+
+exports.sayOwl = (message) => {
+    console.log("              ---- OwlSay ----               ");
+    console.log("                                             ");
+    console.log("             (" + message + ")               ");
+    console.log("           o                                 ");
+    console.log("   ,___, °                                   ");
+    console.log("   [O.o]                                     ");
+    console.log("   /)__)                                     ");
+    console.log("   ╔”═”╗                                     ");
+    console.log("   ║   ║                                     ");
+    console.log("   ║   ║                                     ");
+    console.log("   ╚═══╝                                     ");
+}
+
+exports.sayDoubleOwl = (messageA, messageB) => {
+    console.log("              ---- OwlSay ----               ");
+    console.log("                                             ");
+    console.log("             ( " + messageA + " )            ");
+    console.log("           o                                 ");
+    console.log("   ,___, °               ( " + messageB + " )");
+    console.log("   [O.o]               o                     ");
+    console.log("   /)__)       ,___, °                       ");
+    console.log("   ╔”═”╗       [O.o]                         ");
+    console.log("   ║   ║       /)__)                         ");
+    console.log("   ║   ╚════════”═”═════╗                    ");
+    console.log("   ╚════════════════════╝                    ");
+}
+
+exports.botOwl = () => {
+    console.log("              ---- OwlSay ----               ");
+    console.log("                                             ");
+    console.log("             ( Hi! I'm Owlsay )              ");
+    console.log("           o                                 ");
+    console.log("   ,___, °                                   ");
+    console.log("   [O.o]                                     ");
+    console.log("   /)__)                                     ");
+    console.log("   ╔”═”╗                                     ");
+    console.log("   ║   ║                                     ");
+    console.log("   ║   ║                                     ");
+    console.log("   ╚═══╝                                     ");
+}
+
+exports.botDoubleOwl = () => {
+    console.log("           ---- OwlSay ----                  ");
+    console.log("                                             ");
+    console.log("             ( Oh! Another Owlsay )          ");
+    console.log("           o                                 ");
+    console.log("   ,___, °               (Yes. This is a Olwsayverse)");
+    console.log("   [O.o]               o                     ");
+    console.log("   /)__)       ,___, °                       ");
+    console.log("   ╔”═”╗       [O.o]                         ");
+    console.log("   ║   ║       /)__)                         ");
+    console.log("   ║   ╚════════”═”═════╗                    ");
+    console.log("   ╚════════════════════╝                    ");
+}
+
+exports.thanksOwl = () => {
+    console.log("           ---- OwlSay ----            ");
+    console.log("                                       ");
+    console.log("      (Thanks for using Owlsay)        ");
+    console.log("           o     o      o              ");
+    console.log("   ,___, °       o        ° ,___,      ");
+    console.log("   [O.o]         °          [O.o]      ");
+    console.log("   /)__)       ,___,        /)__)      ");
+    console.log("   ╔”═”╗       [O.o]        ╔”═”╗      ");
+    console.log("   ║   ║       /)__)        ║   ║      ");
+    console.log("   ║   ╚════════”═”═════════╝   ║      ");
+    console.log("   ╚════════════════════════════╝      ");
+}
